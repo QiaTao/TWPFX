79093b865b9a4b939ab9f94eedc2c6a7fa79886d
QiaTao <94186671+QiaTao@users.noreply.github.com>
2025-07-21 23:17:45 +0800

    上传最新状态

---
+++ DashboardPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows.Media;
+using TWPFX.Animations;
+using TWPFX.Controls.Button.SegoeButton;
+using TWPFX.Controls.Icon.SegoeIcon;
+using TWPFX.Controls.Notification.InfoBar;
+using TWPFX_Gallery.ViewModels.Pages;
+using Wpf.Ui.Abstractions.Controls;
+using Wpf.Ui.Controls;
+
+namespace TWPFX_Gallery.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
